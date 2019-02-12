fad8d5b7ff9bb7a8d72b5bd7bf2944859ec4c517
Tyler Hunt <tyhunt04@gmail.com>
2019-02-12 18:09:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,6 +22,14 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, player = "X")
+    @board[index] = player
+  end
+
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
